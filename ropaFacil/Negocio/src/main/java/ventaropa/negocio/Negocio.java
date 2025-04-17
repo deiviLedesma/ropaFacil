213d869bcad39adb250a5c2c1ff0e3acb2118524
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2025-04-16 20:28:51 -0700

    Add files via upload

---
+++ ropaFacil/Negocio/src/main/java/ventaropa/negocio/Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ventaropa.negocio;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ropaFacil/Persistencia/src/main/java/ventaropa/persistencia/Persistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ventaropa.persistencia;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class Persistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
